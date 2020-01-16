a4f2f132fc35305a613710fc6c537e58eae83e5f
Andre Castillo <castilloandre@tutamail.com>
2020-01-16 02:19:38 -0600

    Added button transition on startup, and renamed some things

--- app/src/main/java/com/RNG/MainActivity.java
+++ app/src/main/java/com/RNG/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int screenWidth = displayMetrics.widthPixels;
+
+        Button wheelButton = findViewById(R.id.wheel);
+        wheelButton.setX(screenWidth);
+        wheelButton.animate().x(30);
+
+        Button diceButton = findViewById(R.id.dice);
+        diceButton.setX(-screenWidth - 20);
+        diceButton.animate().x(30);
+
+        Button randomNumberButton = findViewById(R.id.random_number);
+        randomNumberButton.setX(screenWidth);
+        randomNumberButton.animate().x(30);
+
+        Button randomPasswordButton = findViewById(R.id.random_password);
+        randomPasswordButton.setX(-screenWidth - 20);
+        randomPasswordButton.animate().x(30);
     }
 
     public void wheelOfRNG_OnClick(View view) {
